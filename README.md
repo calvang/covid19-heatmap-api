ba34699cd733bcc1f3327b9177e553648add775f
calvang <calvin.huang5@gmail.com>
2020-07-21 01:19:04 -0400

    Complete basic API structure

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Covid 19 Heatmap API
 
-Backend REST API implemented using Rust through the Rocket framework
-
-## Description
+Simple backend REST API implemented using Rust through the Rocket framework. No database was simplemented for this due to the existing python scripts that are scheduled to fetch the data from external API's and sources.
 
 ## Usage
 
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+// extern crate serde;
+// extern crate serde_json;
 // Scheduler, and trait for .seconds(), .minutes(), etc.
 use clokwerk::{Scheduler, TimeUnits};
 // Import week days and WeekDay
 // use clokwerk::Interval::*;
 // use std::thread;
 use std::time::Duration;
+use std::process::Command;
+use std::fs::File;
+use std::io::Read;
+//use rocket::response::content;
+use rocket_contrib::json::Json;
+
+fn run_scripts() {
+    Command::new("./dataset/updateData.sh")
+        .arg("")
+        .output()
+        .expect("Failed to execute data cycle script");
+}
+
+fn parse_counties() -> String {
+    let mut json_file = File::open("dataset/fullCountyData.json").unwrap();
+    let mut json_buffer = String::new();
+    json_file.read_to_string(&mut json_buffer).unwrap();
+    return json_buffer;
+    //let json_data: serde_json::Value = serde_json::from_reader(json_file).unwrap();
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -14,8 +36,8 @@ fn index() -> &'static str {
 }
 
 #[get("/us/counties")]
-fn get_us_counties() -> &'static str {
-    "Hello, world!"
+fn get_us_counties() -> Json<String> {
+    Json(parse_counties())
 }
 
 #[get("/brazil")]
@@ -25,9 +47,10 @@ fn get_brazil_states() -> &'static str {
 
 fn main() {
     let mut scheduler = Scheduler::new();
-    scheduler.every(360.minutes()).run(|| println!("Periodic task"));
-    let thread_handle = scheduler.watch_thread(Duration::from_millis(100));
-    rocket::ignite().mount("/", routes![index]).launch();
-    rocket::ignite().mount("/", routes![get_us_counties]).launch();
-    rocket::ignite().mount("/", routes![get_brazil_states]).launch();
+    scheduler.every(360.minutes()).run(|| run_scripts());
+    let _thread_handle = scheduler.watch_thread(Duration::from_millis(100));
+    rocket::ignite().mount("/", routes![index, get_us_counties, get_brazil_states])
+        .launch();
+    // rocket::ignite().mount("/us/counties", routes![get_us_counties]).launch();
+    // rocket::ignite().mount("/brazil", routes![get_brazil_states]).launch();
 }
